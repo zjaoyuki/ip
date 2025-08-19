5bbab27801d6e9aa204075d9150e7499af767747
zengjing <zjgoalsforlife312@gmail.com>
2025-08-19 22:31:05 +0800

    Added 'Task.java'

--- src/main/java/Rafayel.java
+++ src/main/java/Rafayel.java
@@ -9,7 +9,8 @@ public class Rafayel {
                 + LINE;
         String END_MSG = " Bye. Hope to see you again soon!\n";
 
-        String[] data = new String[100]; // assume less than 100
+        Task[] tasks = new Task[100]; // assume less than 100
+        // String[] data = new String[100];
         int counter = 0;
 
         Scanner sc = new Scanner(System.in);
@@ -23,13 +24,47 @@ public class Rafayel {
             if (input.equals("bye")) {
                 System.out.println(END_MSG);
                 break;
+
             } else if (input.equals("list")) {
+
                 for (int i = 0; i < counter; i++) {
-                    System.out.println(i + 1 + ". " + data[i]);
-                    // System.out.println(String.format("%d. %s", i + 1, data[counter]));
+                    System.out.println(i + 1 + "." + tasks[i].getString());
+                    // System.out.println(String.format("%d. %s", i + 1, data[i]));
+                }
+            } else if (input.length() >= 4 && input.substring(0, 4).equals("mark")) {
+                if (input.length() <= 5) {
+                    System.out.println("Please state what task to be marked as complete.");
+                } else {
+                    String[] temp = input.split(" ");
+                    // check if it is an int -- if (temp[2])
+                    int taskNumber = Integer.parseInt(temp[1]);
+                    if (taskNumber <= 0 && taskNumber >= counter) {
+                        System.out.println("Invalid task number.");
+                    }
+                    taskNumber--;
+
+                    tasks[taskNumber].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n  " + tasks[taskNumber].getString());
+                }
+
+            } else if (input.length() >= 6 && input.substring(0, 6).equals("unmark")) {
+                if (input.length() <= 7) {
+                    System.out.println("Please state what task to be marked as complete.");
+                } else {
+                    String[] temp = input.split(" ");
+                    int taskNumber = Integer.parseInt(temp[1]);
+                    if (taskNumber <= 0 && taskNumber >= counter) {
+                        System.out.println("Invalid task number.");
+                    }
+                    taskNumber--;
+
+                    tasks[taskNumber].markAsUndone();
+                    System.out
+                            .println("OK, I've marked this task as not done yet:\n  " + tasks[taskNumber].getString());
                 }
             } else {
-                data[counter] = input;
+                Task newTask = new Task(input);
+                tasks[counter] = newTask;
                 counter++;
 
                 System.out.println("added: " + input);
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
+    public String getString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
+}
