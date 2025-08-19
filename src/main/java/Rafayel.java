dd213c79bffd05cabdb00ad49b24ac452e3ddcbc
zengjing <zjgoalsforlife312@gmail.com>
2025-08-19 17:52:34 +0800

    Chatbot stores and list items

--- src/main/java/Rafayel.java
+++ src/main/java/Rafayel.java
@@ -7,9 +7,10 @@ public class Rafayel {
                 + " Hello! I'm Rafayel\n"
                 + " What can I do for you?\n"
                 + LINE;
-        String END_MSG = LINE + "\n"
-                + " Bye. Hope to see you again soon!\n"
-                + LINE;
+        String END_MSG = " Bye. Hope to see you again soon!\n";
+
+        String[] data = new String[100]; // assume less than 100
+        int counter = 0;
 
         Scanner sc = new Scanner(System.in);
 
@@ -17,14 +18,23 @@ public class Rafayel {
 
         while (true) {
             String input = sc.nextLine();
+            System.out.println(LINE);
 
             if (input.equals("bye")) {
                 System.out.println(END_MSG);
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < counter; i++) {
+                    System.out.println(i + 1 + ". " + data[i]);
+                    // System.out.println(String.format("%d. %s", i + 1, data[counter]));
+                }
+            } else {
+                data[counter] = input;
+                counter++;
+
+                System.out.println("added: " + input);
             }
 
-            System.out.println(LINE);
-            System.out.println(" " + input);
             System.out.println(LINE);
         }
 
