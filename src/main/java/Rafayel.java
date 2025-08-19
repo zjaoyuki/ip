0149b1e569260024a21d1ffa0fff8db54095cec1
zengjing <zjgoalsforlife312@gmail.com>
2025-08-19 17:35:42 +0800

    Chatbot echos user

--- src/main/java/Rafayel.java
+++ src/main/java/Rafayel.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Rafayel {
     public static void main(String[] args) {
-        String START_MSG = "____________________________________________________________\n" + //
-                "Hello! I'm Rafayel\n" + //
-                " What can I do for you?\n";
-        String END_MSG = "____________________________________________________________\n" + //
-                " Bye. Hope to see you again soon!";
-        System.out.println(START_MSG + END_MSG);
+        String LINE = "____________________________________________________________";
+        String START_MSG = LINE + "\n"
+                + " Hello! I'm Rafayel\n"
+                + " What can I do for you?\n"
+                + LINE;
+        String END_MSG = LINE + "\n"
+                + " Bye. Hope to see you again soon!\n"
+                + LINE;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(START_MSG);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(END_MSG);
+                break;
+            }
+
+            System.out.println(LINE);
+            System.out.println(" " + input);
+            System.out.println(LINE);
+        }
+
+        sc.close();
     }
 }
