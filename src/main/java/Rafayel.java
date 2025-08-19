7cdc1e18534435ab4ed0af78f5cd9a65e2557f0a
zengjing <zjgoalsforlife312@gmail.com>
2025-08-19 17:25:22 +0800

    Renamed Duke.java and modified file

---
+++ src/main/java/Rafayel.java
@@ -0,0 +1,10 @@
+public class Rafayel {
+    public static void main(String[] args) {
+        String START_MSG = "____________________________________________________________\n" + //
+                "Hello! I'm Rafayel\n" + //
+                " What can I do for you?\n";
+        String END_MSG = "____________________________________________________________\n" + //
+                " Bye. Hope to see you again soon!";
+        System.out.println(START_MSG + END_MSG);
+    }
+}
